cb39ad0303ce549c5c0f53e885e39bd5ae2aeaef
Lucifer1331 <162484243+Lucifer1331@users.noreply.github.com>
2024-10-25 02:47:47 +0530

    Update Map.java

--- core/src/main/java/com/angybrids/Map.java
+++ core/src/main/java/com/angybrids/Map.java
@@ -1,16 +1,18 @@
-package com.angybrids;
+package com.angybrids.pages;
 
+import com.angybrids.Button;
+import com.angybrids.Level;
+import com.angybrids.Main;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.Color;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 public class Map implements Screen {
     final Main game;
@@ -24,8 +26,8 @@ public class Map implements Screen {
 
     private Texture mapTitle;
     private Texture background;
+    private Texture levelSelect;
 
-    private ShapeRenderer shapeRenderer;
     private FitViewport viewport;
     private BitmapFont font;
     private boolean visibility;
@@ -40,10 +42,10 @@ public class Map implements Screen {
         backImage = new Texture("icons/homeIcon.png");
         mapTitle = new Texture("map/mapTitle.png");
         background = new Texture("map/mapBG.jpg");
+        levelSelect = new Texture("map/levelSelect.png");
         quitImage = new Texture("icons/exitIcon.png");
         saveImage = new Texture("icons/saveIcon.png");
 
-        shapeRenderer = new ShapeRenderer();
         font = new BitmapFont();
         font.getData().setScale(3.5f);
     }
@@ -73,8 +75,8 @@ public class Map implements Screen {
             Gdx.graphics.getHeight() - 3 * settingImage.getHeight() / 2 -5, 1.2f);
         Button saveButton = new Button(saveImage, Gdx.graphics.getWidth() - settingImage.getWidth() / 2 -35,
             Gdx.graphics.getHeight() - 3 * settingImage.getHeight() / 2 - 25 - quitImage.getHeight(), 1.2f);
-        game.batch.begin();
 
+        game.batch.begin();
         //Map background
         game.batch.draw(background, 0, 0, width, height);
         //Map title
@@ -91,22 +93,28 @@ public class Map implements Screen {
             quitButton.getButtonSprite().draw(game.batch);
             saveButton.getButtonSprite().draw(game.batch);
         }
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.RED);
+        game.batch.end();
+//        shapeRenderer.setColor(Color.RED);
+//        shapeRenderer.setColor(1f, 0.8f, 0.2f, 1f); // honey gold
+//        shapeRenderer.setColor(0.72f, 0.43f, 0.47f, 1f); // rose gold
+        // burnt sienna
         font.setColor(Color.WHITE);
+        ArrayList<Button> levels = new ArrayList<>();
         int x = 240;
         int y = 375;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
-                shapeRenderer.rect(x, y, 200, 150);
+                levels.add(new Button(levelSelect, x, y, 1f));
                 x += 300;
             }
             x = 240;
             y -= 250;
         }
-        game.batch.end();
-        shapeRenderer.end();
+
         game.batch.begin();
+        for(Button b : levels) {
+            b.getButtonSprite().draw(game.batch);
+        }
         x = 230;
         y = 400;
         int level = 1;
@@ -120,24 +128,51 @@ public class Map implements Screen {
             y -= 250;
         }
         game.batch.end();
+
+        //Changing cursor to a pointer while hovering over an icon
+        float touchX = Gdx.input.getX();
+        float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
+        if((settingButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (shopButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (birdButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (backButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (visibility && quitButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (levels.get(0).getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (levels.get(1).getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+         || (levels.get(2).getButtonSprite().getBoundingRectangle().contains(touchX, touchY)))
+            Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand);
+        else if ( (visibility && saveButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+        || (levels.get(3).getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+        || levels.get(4).getButtonSprite().getBoundingRectangle().contains(touchX, touchY)
+         || levels.get(5).getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+            Gdx.graphics.setSystemCursor(Cursor.SystemCursor.NotAllowed);
+        else
+            Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
+
         if (Gdx.input.justTouched()) {
-            // Convert to OpenGL coordinates
-            float touchX = Gdx.input.getX();
-            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
-            if (settingButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
-                if (visibility) {
-                    visibility = false;
-                } else {
-                    visibility = true;
-                }
-            } else {
+            if(visibility && quitButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) System.exit(0);
+//            else if(visibility && saveButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {}
+            if (settingButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY))
+                visibility = !visibility;
+            else {
                 visibility = false;
                 if (backButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
                     game.setScreen(new HomePage(game));
                 } else if (birdButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
                     game.setScreen(new BirdPage(game));
                 } else if (shopButton.getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
-                    game.setScreen(new ShopPage(game));
+                    try {
+                        game.setScreen(new ShopPage(game));
+                    } catch (InvocationTargetException | NoSuchMethodException | InstantiationException |
+                             IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if(levels.get(0).getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+                    game.setScreen(new Level(1, game));
+                } else if(levels.get(1).getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+                    game.setScreen(new Level(2, game));
+                } else if(levels.get(2).getButtonSprite().getBoundingRectangle().contains(touchX, touchY)) {
+                    game.setScreen(new Level(3, game));
                 }
             }
         }
@@ -166,6 +201,7 @@ public class Map implements Screen {
     @Override
     public void dispose() {
         game.batch.dispose();
+
         settingImage.dispose();
         birdImage.dispose();
         shopImage.dispose();
